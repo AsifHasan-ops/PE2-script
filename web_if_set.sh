9ae46689289225a313341addcdadb625783435bd
Asif <54884703+AsifHasan-ops@users.noreply.github.com>
2020-04-22 21:02:39 +0200

    Update web_if_set.sh

--- web_if_set.sh
+++ web_if_set.sh
@@ -1,76 +1,19 @@
 #!/bin/sh
-sudo mkdir -p /home/ubuntu/volumes/web1/web/html
-sudo mkdir -p /home/ubuntu/volumes/web2/web/html
-sudo mkdir -p /home/ubuntu/volumes/web3/web/html
-sudo mkdir -p /home/ubuntu/volumes/lb
-sudo mkdir -p /home/ubuntu/volumes/db1
-sudo mkdir -p /home/ubuntu/volumes/db2
-sudo mkdir -p /home/ubuntu/volumes/db3
-sudo mkdir -p /home/ubuntu/volumes/dbproxy
-sleep 2
-cd /home/ubuntu/volumes/web1/web/html/
-sudo git init 
-sleep 5
-sudo git clone https://github.com/AsifHasan-ops/PE2.git
-sleep  5
-cd PE2
-sudo cp  * ../
-sudo cp  -r * ../
-cd ../
-sudo rm -rf PE2
-sleep 2
-sudo touch /home/ubuntu/volumes/web1/web/html/Dockerfile
-echo "#Dockerfile_web1" > /home/ubuntu/volumes/web1/web/html/Dockerfile
-sleep 2
-sudo sed -i "1 i\FROM richarvey/nginx-php-fpm:latest" /home/ubuntu/volumes/web1/web/html/Dockerfile
-sleep 2
-sudo sed -i '2 i COPY . /var/www/html' /home/ubuntu/volumes/web1/web/html/Dockerfile
-sleep 2
-sudo docker build -t web1 /home/ubuntu/volumes/web1/web/html/.
-sleep 5
-sudo docker run --name=web1  -d web1
-sleep 3
-cd /home/ubuntu/volumes/web2/web/html/
-sudo git init 
-sleep 5
-sudo git clone https://github.com/AsifHasan-ops/PE2.git
-sleep  5
-cd PE2
-sudo cp  * ../
-sudo cp  -r * ../
-cd ../
-sudo rm -rf PE2
+# Set the server's hostname
+myHostName1=web1
+myHostName2=web2
+myHostName3=web3
 
-sudo touch /home/ubuntu/volumes/web2/web/html/Dockerfile
-echo "#Dockerfile_web2" > /home/ubuntu/volumes/web2/web/html/Dockerfile
-sleep 2
-sudo sed -i "1 i\FROM richarvey/nginx-php-fpm:latest" /home/ubuntu/volumes/web2/web/html/Dockerfile
-sleep 2
-sudo sed -i '2 i COPY . /var/www/html' /home/ubuntu/volumes/web2/web/html/Dockerfile
-sleep 2
-sudo docker build -t web2 /home/ubuntu/volumes/web2/web/html/.
-sleep 5
-sudo docker run --name=web2  -d web2
-sleep 3
-cd /home/ubuntu/volumes/web3/web/html/
-sudo git init 
-sleep 5
-sudo git clone https://github.com/AsifHasan-ops/PE2.git
-sleep  5
-cd PE2
-sudo cp  * ../
-sudo cp  -r * ../
-cd ../
-sudo rm -rf PE2
-sleep 2
-sudo touch /home/ubuntu/volumes/web3/web/html/Dockerfile
-echo "#Dockerfile_web3" > /home/ubuntu/volumes/web3/web/html/Dockerfile
-sleep 2
-sudo sed -i "1 i\FROM richarvey/nginx-php-fpm:latest" /home/ubuntu/volumes/web3/web/html/Dockerfile
-sleep 2
-sudo sed -i '2 i COPY . /var/www/html' /home/ubuntu/volumes/web3/web/html/Dockerfile
-sleep 2
-sudo docker build -t web3 /home/ubuntu/volumes/web3/web/html/.
-sleep 5
-sudo docker run --name=web3  -d web3
-sleep 3
+dck=$(docker inspect web1 | grep  '"IPAddress":' | cut -c34-44 > /tmp/a.txt)
+ipaddr1=$(cat /tmp/a.txt  | grep 172.17 | cut -d '"' -f2)
+dck2=$(docker inspect web2 | grep  '"IPAddress":' | cut -c34-44 > /tmp/b.txt)
+ipaddr2=$(cat /tmp/b.txt  | grep 172.17 | cut -d '"' -f2)
+dck3=$(docker inspect web3 | grep  '"IPAddress":' | cut -c34-44 > /tmp/c.txt)
+ipaddr3=$(cat /tmp/c.txt  | grep 172.17 | cut -d '"' -f2)
+
+echo "$ipaddr1 $myHostName1" >> /etc/hosts
+echo "server $myHostName1 $ipaddr1:80" >> /home/ubuntu/volumes/lb/haproxy.cfg
+echo "$ipaddr2 $myHostName2" >> /etc/hosts
+echo "server $myHostName2 $ipaddr2:80" >> /home/ubuntu/volumes/lb/haproxy.cfg
+echo "$ipaddr3 $myHostName3" >> /etc/hosts
+echo "server $myHostName3 $ipaddr3:80" >> /home/ubuntu/volumes/lb/haproxy.cfg
