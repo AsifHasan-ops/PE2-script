6b4206b058ca4e877e5edd916b11667a6142d4a1
Asif <54884703+AsifHasan-ops@users.noreply.github.com>
2020-04-22 20:22:46 +0200

    Update web_if_set.sh

--- web_if_set.sh
+++ web_if_set.sh
@@ -12,8 +12,8 @@ dck3=$(docker inspect web3 | grep  '"IPAddress":' | cut -c34-44 > /tmp/c.txt)
 ipaddr3=$(cat /tmp/c.txt  | grep 172.17 | cut -d '"' -f2)
 
 echo "$ipaddr1 $myHostName1" >> /etc/hosts
-echo "Server $myHostName1 $ipaddr1:80" >> /home/ubuntu/volumes/lb/haproxy.cfg
+echo "server $myHostName1 $ipaddr1:80" >> /home/ubuntu/volumes/lb/haproxy.cfg
 echo "$ipaddr2 $myHostName2" >> /etc/hosts
-echo "Server $myHostName2 $ipaddr2:80" >> /home/ubuntu/volumes/lb/haproxy.cfg
+echo "server $myHostName2 $ipaddr2:80" >> /home/ubuntu/volumes/lb/haproxy.cfg
 echo "$ipaddr3 $myHostName3" >> /etc/hosts
-echo "Server $myHostName3 $ipaddr3:80" >> /home/ubuntu/volumes/lb/haproxy.cfg
+echo "server $myHostName3 $ipaddr3:80" >> /home/ubuntu/volumes/lb/haproxy.cfg
