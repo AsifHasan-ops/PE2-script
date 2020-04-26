280e064f052861d89b48867da67c9b7e0b9552a1
Asif <54884703+AsifHasan-ops@users.noreply.github.com>
2020-04-26 12:44:40 +0200

    Update mariadb-galera.sh

--- mariadb-galera.sh
+++ mariadb-galera.sh
@@ -74,14 +74,14 @@ db3
 sleep 5
 docker exec -it db1 mysql -uroot -prootpass -e "SHOW STATUS LIKE 'wsrep_cluster_size'";
 sleep 2
-sudo curl  https://raw.githubusercontent.com/AsifHasan-ops/PE2-script/master/mariadb-galera-if-set.sh > /home/ubuntu/mariadb-galera-if-set.sh
-sudo chmod +x /home/ubuntu/mariadb-galera-if-set.sh
-/home/ubuntu/mariadb-galera-if-set.sh
-
 docker restart db2 
 sleep 5
 docker restart db3
 
 docker exec -it db1 mysql -uroot -prootpass -e "SHOW STATUS LIKE 'wsrep_cluster_size'";
+
+sudo curl  https://raw.githubusercontent.com/AsifHasan-ops/PE2-script/master/mariadb-galera-if-set.sh > /home/ubuntu/mariadb-galera-if-set.sh
+sudo chmod +x /home/ubuntu/mariadb-galera-if-set.sh
+/home/ubuntu/mariadb-galera-if-set.sh
 
 
