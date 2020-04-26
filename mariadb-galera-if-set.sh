50f136b3dbcd919b5ad5a1e6c504b61a9801d656
Asif <54884703+AsifHasan-ops@users.noreply.github.com>
2020-04-26 11:33:28 +0200

    Update mariadb-galera-if-set.sh

--- mariadb-galera-if-set.sh
+++ mariadb-galera-if-set.sh
@@ -6,9 +6,9 @@ myHostName3=dbgc3
 
 dck=$(docker inspect db1 | grep  '"IPAddress":' | cut -c34-44 > /tmp/d.txt)
 ipaddr1=$(cat /tmp/d.txt  | grep 172.17 | cut -d '"' -f2)
-dck2=$(docker inspect db1 | grep  '"IPAddress":' | cut -c34-44 > /tmp/e.txt)
+dck2=$(docker inspect db2 | grep  '"IPAddress":' | cut -c34-44 > /tmp/e.txt)
 ipaddr2=$(cat /tmp/e.txt  | grep 172.17 | cut -d '"' -f2)
-dck3=$(docker inspect db1 | grep  '"IPAddress":' | cut -c34-44 > /tmp/f.txt)
+dck3=$(docker inspect db3 | grep  '"IPAddress":' | cut -c34-44 > /tmp/f.txt)
 ipaddr3=$(cat /tmp/f.txt  | grep 172.17 | cut -d '"' -f2)
 
 echo "$ipaddr1 $myHostName1" >> /etc/hosts
